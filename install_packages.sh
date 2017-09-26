f97e8fe3d89db13e7b9ff3fd5ed5c8fc436cdbe1
Brian Yee <bay001@tscc-1-43.sdsc.edu>
2017-09-26 14:41:39 -0700

    0.0.5

--- install_packages.sh
+++ install_packages.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+conda create -y -n rbp-maps python=2.7;
+source activate rbp-maps;
+conda install -y numpy=1.12.1 pandas=0.20.3 matplotlib=2.0.2 scipy=0.19.1 seaborn=0.8;
+conda install -y -c bioconda bedtools=2.26.0 pybedtools=0.7.8 samtools=1.3.1 pysam=0.8.4 pybigwig=0.3.5;
+
+python setup.py build;
+python setup.py install;
+
+plot_density
